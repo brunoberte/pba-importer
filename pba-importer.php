db71b40ad9c76b8b1334d50c6c2a910ff6df1599
Bruno Berte <brunoberte@gmail.com>
2013-08-21 11:50:29 -0300

    Testing WP_GitHub_Updater

--- pba-importer.php
+++ pba-importer.php
@@ -36,7 +36,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/brunoberte/pba-importer/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/brunoberte/pba-importer', // the github url of your github repo
         'zip_url' => 'https://github.com/brunoberte/pba-importer/zipball/master', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.5', // which version of WordPress is your plugin tested up to?
         'readme' => 'README.md', // which file to use as the readme for the version number
